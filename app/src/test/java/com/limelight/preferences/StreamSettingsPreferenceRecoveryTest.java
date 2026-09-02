aceedffc013c52817c38b59417dd6fbde880b124
juliekeygen-netizen <julie.keygen@gmail.com>
2026-09-02 21:43:15 +0300

    test: reproduce settings UI type corruption

---
+++ app/src/test/java/com/limelight/preferences/StreamSettingsPreferenceRecoveryTest.java
@@ -0,0 +1,57 @@
+package com.limelight.preferences;
+
+import static org.junit.Assert.assertNotNull;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.preference.PreferenceManager;
+import androidx.test.core.app.ApplicationProvider;
+
+import com.limelight.R;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.android.controller.ActivityController;
+import org.robolectric.annotation.Config;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = 33)
+public class StreamSettingsPreferenceRecoveryTest {
+    private SharedPreferences basePrefs;
+
+    @Before
+    public void setUp() {
+        Context context = ApplicationProvider.getApplicationContext();
+        basePrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        basePrefs.edit().clear().commit();
+    }
+
+    @After
+    public void tearDown() {
+        basePrefs.edit().clear().commit();
+    }
+
+    @Test
+    public void wrongTypedBaseBooleanDoesNotCrashSettingsInflation() {
+        basePrefs.edit()
+                .putString(PreferenceConfiguration.ENABLE_PIP_PREF_STRING, "corrupt")
+                .commit();
+
+        try (ActivityController<StreamSettings> controller = Robolectric.buildActivity(StreamSettings.class)) {
+            StreamSettings activity = controller.create().start().resume().get();
+            StreamSettings.SettingsFragment fragment = new StreamSettings.SettingsFragment();
+
+            activity.getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.stream_settings, fragment)
+                    .commitNow();
+
+            assertNotNull(fragment.findPreference(PreferenceConfiguration.ENABLE_PIP_PREF_STRING));
+        }
+    }
+}
